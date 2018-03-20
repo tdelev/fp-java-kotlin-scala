aaab47826a98b21f304c579f42780bcfb60da81a
Tomche Delev <tomce.delev@gmail.com>
2018-03-21 00:33:49 +0100

    Initial setup of Java and Kotlin gradle project

---
+++ fp-java-kotlin/src/main/java/com/sorsix/fpjava/Functions.java
@@ -0,0 +1,12 @@
+package com.sorsix.fpjava;
+
+public class Functions {
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2));
+    }
+}
---
+++ fp-java-kotlin/src/main/kotlin/com/sorsix/fpkotlin/functions.kt
@@ -0,0 +1,7 @@
+package com.sorsix.fpkotlin
+
+fun sum(a: Int, b: Int) = a + b
+
+fun main(args: Array<String>) {
+    println(sum(2, 3))
+}
