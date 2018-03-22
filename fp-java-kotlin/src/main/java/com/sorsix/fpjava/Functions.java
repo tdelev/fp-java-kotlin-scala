34151084d8e174176f109d73a9513b9ae45fcb45
Tomche Delev <tomche.delev@sorsix.com>
2018-03-22 12:01:40 +0100

    HOF, partial application, curring and composition

--- fp-java-kotlin/src/main/java/com/sorsix/fpjava/Functions.java
+++ fp-java-kotlin/src/main/java/com/sorsix/fpjava/Functions.java
@@ -1,12 +1,85 @@
 package com.sorsix.fpjava;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class Functions {
+
+    /**
+     * Functions are a thing
+     */
 
     static int sum(int a, int b) {
         return a + b;
     }
 
+    static BiFunction<Integer, Integer, Integer> sumF = Functions::sum;
+
+    /**
+     * Partial applications
+     */
+
+    static int sum5(int a) {
+        return sum(5, a);
+    }
+
+    static Function<Integer, Integer> sum5Partial = a -> sumF.apply(5, a);
+
+    static <A, B, C> Function<B, C> partial(A a, BiFunction<A, B, C> f) {
+        return b -> f.apply(a, b);
+    }
+
+    static Function<Integer, Integer> sum10Partial = partial(10, sumF);
+
+    /**
+     * Curring
+     */
+
+    static Function<Integer, Integer> sumA(int a) {
+        return b -> sum(a, b);
+    }
+
+    static <A, B, C> Function<A, Function<B, C>> curry(BiFunction<A, B, C> f) {
+        return a -> b -> f.apply(a, b);
+    }
+
+    static Function<Integer, Function<Integer, Integer>> sumACurried = curry(sumF);
+
+    /**
+     * Composition
+     */
+    static String result(int a) {
+        return String.format("Result is: %d", a);
+    }
+
+    static String resultSum(int a, int b) {
+        return result(sum(a, b));
+    }
+
+    static BiFunction<Integer, Integer, String> resultComposed = sumF.andThen(Functions::result);
+
+    static <A, B, C> Function<A, C> compose(Function<B, C> f, Function<A, B> g) {
+        return a -> f.apply(g.apply(a));
+        // return g.andThen(f);
+    }
+
+    static Function<Integer, String> square = compose(Functions::result, a -> a * a);
+
     public static void main(String[] args) {
         System.out.println(sum(1, 2));
+
+        System.out.println(sumF.apply(5, 10));
+
+        System.out.println(sum5Partial.apply(20));
+
+        System.out.println(sum10Partial.apply(30));
+
+        System.out.println(sumA(10).apply(20));
+
+        System.out.println(sumACurried.apply(10).apply(20));
+
+        System.out.println(resultComposed.apply(15, 20));
+
+        System.out.println(square.apply(10));
     }
 }
--- fp-java-kotlin/src/main/kotlin/com/sorsix/fpkotlin/functions.kt
+++ fp-java-kotlin/src/main/kotlin/com/sorsix/fpkotlin/functions.kt
@@ -1,7 +1,66 @@
 package com.sorsix.fpkotlin
 
+/**
+ * Functions are a thing
+ */
+
 fun sum(a: Int, b: Int) = a + b
+
+val sumF = ::sum
+
+/**
+ * Partial applications
+ */
+
+fun sum5(a: Int): Int {
+    return sum(5, a)
+}
+
+val sum5Partial = { a: Int -> sumF(5, a) }
+
+fun <A, B, C> partial(a: A, f: (A, B) -> C): (B) -> C = { b -> f(a, b) }
+
+val sum10Partial = partial(10, sumF)
+
+/**
+ * Curring
+ */
+
+val sumA = { a: Int -> { b: Int -> sumF(a, b) } }
+
+fun <A, B, C> curry(f: (A, B) -> C): (A) -> (B) -> C =
+        { a -> { b -> f(a, b) } }
+
+fun <A, B, C> ((A, B) -> C).curried(): (A) -> (B) -> C =
+        curry(this)
+
+val sumCurried = curry(sumF)
+
+/**
+ * Composition
+ */
+fun result(a: Int) = "Result is: $a"
+
+fun resultSum(a: Int, b: Int): String {
+    return result(sum(a, b))
+}
+
+fun <A, B, C> compose(f: (B) -> C, g: (A) -> B): (A) -> C {
+    return { a -> f(g(a)) }
+}
+
+val square = compose(::result, { a: Int -> a * a })
 
 fun main(args: Array<String>) {
     println(sum(2, 3))
+
+    println(sumF(5, 10))
+
+    println(sumA(10)(20))
+
+    println(sumCurried(10)(20))
+
+    println(sumF.curried()(10)(30))
+
+    println(square(20))
 }
